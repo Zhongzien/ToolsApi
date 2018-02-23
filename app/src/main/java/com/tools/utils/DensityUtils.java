dde906e5ab6af123e2299e959da2b15f50ca77a4
zze <394583465@qq.com>
2018-02-23 14:04:43 +0800

    新增DensityUtils工具了

---
+++ app/src/main/java/com/tools/utils/DensityUtils.java
@@ -0,0 +1,44 @@
+package com.tools.utils;
+
+import android.content.Context;
+
+/**
+ * Created by zze on 23/2/18.
+ */
+
+public class DensityUtils {
+    private DensityUtils() {
+    }
+
+    /**
+     * 根据手机分辨率从DP转成PX
+     */
+    public static int dip2px(Context context, float dpValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率PX(像素)转成DP
+     */
+    public static int px2dip(Context context, float pxValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+}
