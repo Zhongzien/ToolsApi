a1bd19c6144f441a99c4c8a6e6e2e340010e7aa6
zze <394583465@qq.com>
2018-02-23 12:37:51 +0800

    新增Screen工具了

---
+++ app/src/main/java/com/tools/utils/ScreenUtils.java
@@ -0,0 +1,115 @@
+package com.tools.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by zze on 23/2/18.
+ */
+
+public class ScreenUtils {
+    private ScreenUtils() {
+    }
+
+    public static int getScreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(metrics);
+        return metrics.widthPixels;
+    }
+
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics metrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(metrics);
+        return metrics.heightPixels;
+    }
+
+    /**
+     * point.x 代表 screen width ; point.y 代表 screen height
+     *
+     * @param context
+     * @return
+     */
+    public static Point getScreenSize(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Point point = new Point();
+        wm.getDefaultDisplay().getSize(point);
+        return point;
+    }
+
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param ctx
+     * @return
+     */
+    public static int getStatusH(Activity ctx) {
+        Rect s = new Rect();
+        ctx.getWindow().getDecorView().getWindowVisibleDisplayFrame(s);
+        return s.top;
+    }
+
+    /**
+     * 获取状态栏高度
+     * 反射方式获取
+     *
+     * @param ctx
+     * @return
+     */
+    public static int getStatusH(Context ctx) {
+        int statusHeight = -1;
+        try {
+            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height")
+                    .get(object).toString());
+            statusHeight = ctx.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusHeight;
+    }
+
+    public static int getStatusHeight(Activity activity) {
+        int resourceId = activity.getResources()
+                .getIdentifier("status_bar_height", "dimen", "android");
+        return resourceId > 0 ? activity.getResources().getDimensionPixelSize(resourceId) : 0;
+    }
+
+    /**
+     * 获取标题栏高度
+     *
+     * @param ctx
+     * @return
+     */
+    public static int getTitleH(Activity ctx) {
+        int contentTop = ctx.getWindow()
+                .findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        return contentTop - getStatusH(ctx);
+    }
+
+    /**
+     * 获取底部领航栏高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getNavigationBarHeight(Context context) {
+        int navigationBarHeight = -1;
+        Resources resources = context.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            navigationBarHeight = resources.getDimensionPixelSize(resourceId);
+        }
+        return navigationBarHeight;
+    }
+
+}
